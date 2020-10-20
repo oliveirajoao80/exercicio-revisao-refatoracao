2de76ddc0f319ba56a929c71c3065f13ea867628
oliveirajoao80 <73171930+oliveirajoao80@users.noreply.github.com>
2020-10-20 08:19:22 -0300

    2019027644

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,25 @@
 #include <string>
 #include "Empregado.hpp"
 
+#define MESESREGULMENTADOS 12
+
 class Vendedor : public Empregado {
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
-};
+    public:
 
+        double quotaMensalVendas;
+
+        double quotaTotalAnual() {
+
+            return (quotaMensalVendas * MESESREGULMENTADOS);
+        }
+
+        void print(){
+
+            std::cout << "Nome: " << nome << std::endl;
+            std::cout << "Salario Mes: " << pagamentoMes(horasTrabalhadas) << std::endl;
+            std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl;
+            std::cout << std::endl;
+
+        }
+};
