dc620773ed7b35a27becbb4cf0e4f12ae29f3fdf
oliveirajoao80 <73171930+oliveirajoao80@users.noreply.github.com>
2020-10-20 08:17:43 -0300

    2019027644
    alterei as indentações
    adicionei a função print 
    adicionei uma string nome e uma variável no arquivo Empregado.cpp
    tirei os números mágicos, defini como constantes com nomes sugestivos
    renomeei algumas variáveis com nomes mais sugestivos
    movi double quotaMensalVendas para o arquivo Vendedor.cpp
    retirei os cout da main

--- Empregado.hpp
+++ Empregado.hpp
@@ -4,25 +4,31 @@
 #include <iostream>
 #include <string>
 
+#define HORARIOCOMERCIAL 8
+#define PESOHORAEXTRA 0.5
+
 class Empregado {
-	
-  public:
-    double salarioHora;  
-    double quotaMensalVendas;  
 
+    public:
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
-	
+        std::string nome;
+
+        double horasTrabalhadas;
+
+        double salarioHora;
+
+        double pagamentoMes(double horasTrabalhadas) {
+
+            double TotHora = horasTrabalhadas;
+                if (horasTrabalhadas > HORARIOCOMERCIAL) {
+                    double HoraExtra;
+                        HoraExtra = (horasTrabalhadas - HORARIOCOMERCIAL);
+                            TotHora += (HoraExtra * PESOHORAEXTRA);
+                }
+                        return (TotHora * salarioHora);
+
+        }
+
 };
 
 #endif
