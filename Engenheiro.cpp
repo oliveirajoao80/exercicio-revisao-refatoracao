f75b326ec38f53d4b968a85582d931be49cd16ca
oliveirajoao80 <73171930+oliveirajoao80@users.noreply.github.com>
2020-10-20 08:18:33 -0300

    2019027644

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,20 @@
 #include <string>
 #include "Empregado.hpp"
 
+
 class Engenheiro : public Empregado {
 
   public:
-    std::string nome;  
-	int projetos;
-	
-};
 
+	int ProjetosTotais;
+
+    void print(){
+
+       std::cout << "Nome: " << nome << std::endl;
+       std::cout << "Salario mes " << pagamentoMes(horasTrabalhadas) << std::endl;
+       std::cout << "Projetos: " << ProjetosTotais << std::endl;
+       std::cout << std::endl;
+  }
+
+
+};
